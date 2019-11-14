8883b8d46df1b813c0de76eb4c7d865845fb0ff9
yChar <cdanielhdezperez@gmail.com>
2019-11-14 03:04:04 -0600

    Se han mejorado los forms y las vistas

--- apps/crud/forms.py
+++ apps/crud/forms.py
@@ -3,11 +3,12 @@ from django import forms
 #importamos el modelo de persona
 from .models import Persona
 
+input_attrs = {'class' : 'form-control text-right mb-2'}
+
 class EmailForm(forms.Form):
-    email = forms.CharField(label='Correo Electrónico', max_length=100,widget=forms.TextInput(attrs={'class' : 'form-control' }))
+    email = forms.CharField(label='Correo Electrónico', max_length=30,widget=forms.TextInput(input_attrs))
 
 class RegistroPersonaForm(forms.Form):
-    model = Persona
-    widgets = {
-        'nombre' :  forms.TextInput(attrs={'class' : 'form-control'})
-    }
+    nombre = forms.CharField(label='Nombre', max_length=50,widget=forms.TextInput(input_attrs))
+    a_paterno = forms.CharField(label='Apellido Paterno', max_length=50,widget=forms.TextInput(input_attrs))
+    a_materno = forms.CharField(label='Apellido Materno', max_length=50,widget=forms.TextInput(input_attrs))
